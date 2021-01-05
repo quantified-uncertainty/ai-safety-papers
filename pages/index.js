6d169f019edac23587ec9c0402b43a2469954ba5
Ozzie Gooen <ozzieagooen@gmail.com>
2021-01-05 00:08:43 -0800

    Simple layout and stuff

--- pages/index.js
+++ pages/index.js
@@ -1,87 +1,35 @@
-import Head from "next/head";
 import Link from "next/link";
-import Airtable from "airtable";
 import ReactMarkdown from "react-markdown";
-import { getTable, getRecord } from "../lib/airtable.js";
+import { getCauseCandidates } from "../lib/airtablegraph.js";
+import Layout from "./layout.js";
 
 export async function getStaticProps() {
-  const records = await getTable("Narrow Cause Area")
-    .select({
-      fields: ["Name", "Description", "Image", "General Rating"],
-    })
-    .all();
-
-  const products = records.map((p) => {
-    let get = getRecord["Narrow Cause Area"];
-    return {
-      id: get["id"](p),
-      name: get["name"](p),
-      description: get["description"](p),
-      image: get["image"](p),
-    };
-  });
-
+  const { causeCandidates } = await getCauseCandidates();
   return {
     props: {
-      products,
+      items: causeCandidates,
     },
   };
 }
 
-let person = ({ src, name, description, rating, id }) => (
-  <div key={id}>
-    <td className="px-6 py-4 whitespace-nowrap">
-      <div className="flex items-center">
-        <div className="flex-shrink-0 h-10 w-10">
-          {src ? (
-            <img className="h-10 w-10 rounded" src={src} alt="" />
-          ) : (
-            <div />
-          )}
-        </div>
-        <div className="ml-4">
-          <Link href={`/posts/${id}`} passHref>
-            <span className="text-sm font-medium text-blue-600 hover:text-blue-800 visited:text-blue-800 hover:underline cursor-pointer">
-              {name}
-            </span>
-          </Link>
-        </div>
-      </div>
-    </td>
-    <td className="mt-1 text-sm text-gray-500 sm:mt-0 sm:col-span-2">
-      <ReactMarkdown>{description}</ReactMarkdown>
-    </td>
-  </div>
-);
-
-export default function Home({ products }) {
+let causeCandidate = ({ id, name, totalKarmaDiscussing, causeArea }) => {
   return (
-    <div>
-      <Head>
-        <title>Create Next App!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <div className="container px-6 mx-auto grid">
-          <div className="w-full overflow-hidden rounded-lg shadow">
-            <div className="w-full overflow-x-auto">
-              <div className="bg-white divide-y divide-gray-200">
-                {products.map((product) =>
-                  person({
-                    id: product.id,
-                    src: product.name,
-                    name: product.name,
-                    description: product.description,
-                    src: product.image,
-                    rating: product.rating,
-                  })
-                )}
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
+    <div key={id} className="pb-4">
+      <Link href={`/posts/${id}`} passHref>
+        <span className="text-sm font-medium text-blue-600 hover:text-blue-800 visited:text-blue-800 hover:underline cursor-pointer">
+          {name}
+        </span>
+      </Link>
+      <span className="mt-1 sm:mt-0 sm:col-span-2 text-gray-700 bg-gray-200 hover:bg-gray-400 hover:text-gray-900 px-1 py-1 rounded-sm text-sm cursor-pointer">
+        {causeArea[0].name}
+      </span>
+      <div className="mt-1 text-sm text-gray-500 sm:mt-0 sm:col-span-2">
+        <ReactMarkdown>{totalKarmaDiscussing}</ReactMarkdown>
+      </div>
     </div>
   );
+};
+
+export default function Home({ items }) {
+  return <Layout key="index">{items.map((i) => causeCandidate(i))}</Layout>;
 }
---
+++ pages/layout.js
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import Head from "next/head";
+
+export default function Layout(props) {
+  return (
+    <div>
+      <Head>
+        <title>Cause Explorer</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>
+        <nav className="bg-gray-800">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex items-center justify-between h-16">
+              <div className="flex items-center">
+                <div className="flex-shrink-0">
+                  <img
+                    className="h-8 w-8"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    alt="Workflow"
+                  />
+                </div>
+                <div className="hidden md:block">
+                  <div className="ml-10 flex items-baseline space-x-4">
+                    <Link href={`/`} passHref>
+                      <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer">
+                        Causes
+                      </span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <main>
+          <div className="container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="py-8">{props.children}</div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
