3bcb0b138615cfe69cf04014bd5accfc8da65a3b
Ozzie Gooen <ozzieagooen@gmail.com>
2021-02-19 19:03:19 -0800

    Change markdown provider

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import { getPapers } from "../lib/airtablegraph.js";
 import Layout from "./layout.js";
-import ReactMarkdown from "react-markdown";
 import Fuse from "fuse.js";
 import React, { useState, useEffect, useReducer, useRef } from "react";
 import { useHotkeys } from "react-hotkeys-hook";
@@ -11,6 +10,12 @@ import { ImBook } from "react-icons/im";
 import { IoIosBook } from "react-icons/io";
 import { MdBookmark } from "react-icons/md";
 import { debounce } from "lodash";
+import markdownIt from "markdown-it";
+import markdownItMathjax from "./lib/markdown-mathjax";
+import { markdownItSub } from "markdown-it-sub";
+
+const mdi = markdownIt({ linkify: true });
+mdi.use(markdownItMathjax());
 
 export async function getStaticProps() {
   const { papers } = await getPapers();
@@ -96,58 +101,60 @@ function PaperListView(props) {
   );
 }
 
-let blurb = ({ sourceName, sourceLink, isStarred, blurb }) => (
-  <div className="bg-gray-50 border border-gray-200 rounded-md mb-6">
-    <div className="border-b border-gray-200 px-4 py-3 flex justify-between">
-      <div className="flex justify-start items-center text-gray-700">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="flex-shrink-0 w-5 h-5"
+let blurb = ({ sourceName, sourceLink, isStarred, blurb }) => {
+  return (
+    <div className="bg-gray-50 border border-gray-200 rounded-md mb-6">
+      <div className="border-b border-gray-200 px-4 py-3 flex justify-between">
+        <div className="flex justify-start items-center text-gray-700">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="flex-shrink-0 w-5 h-5"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <div className="flex-1 ml-2 text-sm font-bold"> {sourceName}</div>
+        </div>
+        <a
+          href={sourceLink}
+          className="justify-end flex items-center text-gray-400 hover:underline hover:text-gray-500"
         >
-          <path
-            fill-rule="evenodd"
-            d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7"
-            clip-rule="evenodd"
-          />
-        </svg>
-        <div className="flex-1 ml-2 text-sm font-bold"> {sourceName}</div>
+          <div className="flex-1 mr-1 text-sm">Link</div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="flex-shrink-0 w-5 h-4"
+          >
+            <path
+              fillRule="evenodd"
+              d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </a>
       </div>
-      <a
-        href={sourceLink}
-        className="justify-end flex items-center text-gray-400 hover:underline hover:text-gray-500"
-      >
-        <div className="flex-1 mr-1 text-sm">Link</div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="flex-shrink-0 w-5 h-4"
-        >
-          <path
-            fillRule="evenodd"
-            d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </a>
-    </div>
-    {isStarred && (
-      <div className="border-b border-gray-200 bg-yellow-50 px-4 pt-2 pb-1">
-        <span className="inline-flex items-center text-yellow-500">
-          <span className="text-yellow-500 text-md mr-2">
-            <AiFillStar />
+      {isStarred && (
+        <div className="border-b border-gray-200 bg-yellow-50 px-4 pt-2 pb-1">
+          <span className="inline-flex items-center text-yellow-500">
+            <span className="text-yellow-500 text-md mr-2">
+              <AiFillStar />
+            </span>
+            <span className="text-sm font-bold">Highlight</span>
           </span>
-          <span className="text-sm font-bold">Highlight</span>
-        </span>
+        </div>
+      )}
+      <div className="prose p-4 bg-neutral-100 text-neutral-600 mt-1 mb-2 max-w-5xl text-gray-700">
+        <div dangerouslySetInnerHTML={{ __html: mdi.render(blurb) }} />
       </div>
-    )}
-    <div className="prose p-4 bg-neutral-100 text-neutral-600 mt-1 mb-2 max-w-5xl text-gray-700">
-      <ReactMarkdown source={cleanMarkdown(blurb)} />
     </div>
-  </div>
-);
+  );
+};
 
 let cleanMarkdown = (r) => r.replaceAll("\\", "");
 let paperPageView = ({
@@ -196,9 +203,11 @@ let paperPageView = ({
       </div>
       {abstractNote && (
         <div className="prose text-gray-700 bg-neutral-100 text-neutral-600 mt-4 mb-5 max-w-5xl">
-          <ReactMarkdown>
-            {(abstractNote && cleanMarkdown(abstractNote)) || ""}
-          </ReactMarkdown>
+          {abstractNote && (
+            <div
+              dangerouslySetInnerHTML={{ __html: mdi.render(abstractNote) }}
+            />
+          )}
         </div>
       )}
 
@@ -233,15 +242,6 @@ const opts = {
     "orgs",
     "anBlurb",
   ],
-};
-
-const initialQuery = {
-  query: "",
-  typeBlogPost: true,
-  typeManuscript: true,
-  typeConferencePaper: true,
-  typeJournalArticle: true,
-  typeReport: true,
 };
 
 // TODO: use this for search:
