335be51beff8d6412aacf98482ec2494d4a70065
NunoSempere <nuno.sempere@gmail.com>
2021-09-09 11:52:57 +0200

    tweak: Clarify the up-to-dateness of the table

--- pages/table.js
+++ pages/table.js
@@ -7,7 +7,10 @@ export default function Table() {
         <h1 className="text-xl font-bold text-gray-600">Table View</h1>
         <p className="text-gray-600">
           This is the complete Airtable table. You can use this to copy the CSV
-          if you like. Note that it can take a few seconds to load.
+          if you like. Note that it can take a few seconds to load. Note also that
+          unlike the search, the contents are up to date with the
+          <a href="https://www.lesswrong.com/posts/4DegbDJJiMX2b3EKm/tai-safety-bibliographic-database"> TAI Safety Bibliographic Database</a>,
+          but not with the <a href="http://rohinshah.com/alignment-newsletter/">Alignment Newsletter</a>
         </p>
       </div>
       <iframe
